7026d636802521d808b16a09a3d7c2e7739a6d02
Jitendra <jitendra.takalkar@gmail.com>
2011-10-16 22:03:22 +0200

    aded bootstrap css file

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
                  [ring/ring-jetty-adapter "0.3.11"]
                  [postgresql/postgresql "8.4-702.jdbc4"]
                  [compojure "0.6.4"]
-                 [congomongo "0.1.7"]]               
+                 [congomongo "0.1.7"]
                  [hiccup "0.3.6"]]               
   :dev-dependencies [[swank-clojure "1.3.3"]
                      [lein-ring "0.4.5"]]
